fcd6707a04d86b26cd5bb1f6cb773fc5daa491bc
MorganMusibi <morganmusibi@gmail.com>
2026-07-21 03:40:09 +0300

    Update Profile.tsx

--- artifacts/bizmtaani/src/pages/Profile.tsx
+++ artifacts/bizmtaani/src/pages/Profile.tsx
@@ -1,4 +1,4 @@
-import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { ref, uploadBytes, getDownloadURL, deleteObject, } from "firebase/storage";
 import { db, storage } from "@/lib/firebase";
 import { doc, updateDoc } from "firebase/firestore";
 import { useRef, useState } from "react";
