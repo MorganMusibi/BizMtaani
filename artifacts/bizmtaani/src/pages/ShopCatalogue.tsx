e63b747549b63fd0959c8eb000f84e4de865d284
MorganMusibi <morganmusibi@gmail.com>
2026-08-03 07:37:10 +0300

    Fix collection name from 'products' to 'Products'

--- artifacts/bizmtaani/src/pages/ShopCatalogue.tsx
+++ artifacts/bizmtaani/src/pages/ShopCatalogue.tsx
@@ -313,11 +313,11 @@ export default function ShopCatalogue() {
       try {
         setLoading(true);
 
-        const productsQuery =
+                const productsQuery =
           query(
             collection(
               db,
-              "products"
+              "Products"
             ),
             where(
               "sellerId",
