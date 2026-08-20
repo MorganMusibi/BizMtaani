9b5f6f6f8f127b16c9009c36cbfac3764955a812
MorganMusibi <morganmusibi@gmail.com>
2026-08-20 06:19:23 +0300

    Fix import statement for firebase/functions

--- artifacts/bizmtaani/src/lib/uploadImage.ts
+++ artifacts/bizmtaani/src/lib/uploadImage.ts
@@ -1,4 +1,4 @@
-.import { getFunctions, httpsCallable } from "firebase/functions";
+import { getFunctions, httpsCallable } from "firebase/functions";
 import { app } from "@/lib/firebase";
 import imageCompression from "browser-image-compression";
 
